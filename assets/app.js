bfb2670e646644ec929f9b19abae5891c9d8ab11
michael <michael.green94@hotmail.com>
2018-11-09 18:59:39 -0700

    findIndex

--- assets/app.js
+++ assets/app.js
@@ -11,4 +11,29 @@ function reverse(str) {
   }
   return out
 }
-console.log(reverse("Hello"))
+console.log(reverse("Hello World"))
+console.clear()
+
+// Create a function that takes an array and a string as arguments and return the index of the string.
+
+function findIndex(arr, str) {
+  for (let i = 0; i < arr.length; i++) {
+    let string = arr[i]
+    console.log(string)
+    if (str == string) {
+      return i
+    }
+  }
+}
+console.log(findIndex(['a', 'b', 'c', 'd', 'e', 'f'], 'f'))
+console.clear()
+
+// // Create a function that takes a string and returns the word count. The string will be a sentence.
+
+// function wordCount(str) {
+//   // let out = str.split(' ')
+//   // console.log(out)
+//   // return out.length
+//   return str.split(' ').length
+// }
+// console.log(wordCount("Is this easy mode"))
